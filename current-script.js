7b2fe48c65dcf7059404f22c4264c1c9cb146783
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2025-10-13 16:11:26 +0300

    working file

---
+++ current-script.js
@@ -0,0 +1,31 @@
+function syncFromExternalSheet() {
+  const externalSheetId = '1oOjqXsaCAjSOkA1lXrasNtUVtrsxvyxcFcolD8n6YXY';
+  const externalSheetName = 'LOC1';
+
+  const externalSource =
+    SpreadsheetApp.openById(externalSheetId).getSheetByName(externalSheetName);
+
+  const mySheetId = '1SQi7OMWiVyim7HTO7UhgdDZuzsJZAIXj2imoCiTAqRs';
+  const mySheetName = 'Sheet1';
+
+  const mySheet =
+    SpreadsheetApp.openById(mySheetId).getSheetByName(mySheetName);
+
+  const lastRow = externalSource.getLastRow();
+  const numRows = lastRow - 8;
+  const data = externalSource.getRange(9, 2, numRows, 2).getValues();
+
+  for (let i = 0; i < data.length; i++) {
+    data[i][1] = replaceStockStatus(data[i][1]);
+  }
+
+  mySheet.getRange(1, 1, data.length, data[0].length).setValues(data);
+
+  function replaceStockStatus(value) {
+    if (value === 'IN STOCK') {
+      return 9999;
+    } else {
+      return 0;
+    }
+  }
+}
