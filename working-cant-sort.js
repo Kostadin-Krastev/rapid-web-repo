e369674522018891b6f6d7979a4392865e5e6d24
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2025-10-13 14:38:53 +0300

    pull data but cant sort the sku

---
+++ working-cant-sort.js
@@ -0,0 +1,43 @@
+function syncFromExternalSheet() {
+  const externalSheetId = '1oOjqXsaCAjSOkA1lXrasNtUVtrsxvyxcFcolD8n6YXY';
+  const externalSheetName = 'LOC1';
+  const externalSource =
+    SpreadsheetApp.openById(externalSheetId).getSheetByName(externalSheetName);
+
+  const mySheetId = '1SQi7OMWiVyim7HTO7UhgdDZuzsJZAIXj2imoCiTAqRs';
+  const mySheetName = 'Sheet1';
+  const mySheet =
+    SpreadsheetApp.openById(mySheetId).getSheetByName(mySheetName);
+
+  const lastRow = externalSource.getLastRow();
+  const numRows = lastRow - 8;
+  let data = externalSource.getRange(9, 2, numRows, 2).getValues();
+
+  data = data.filter((row) => row[0] && row[0].toString().trim() !== ''); // премахвам празните редове в таблицата
+
+  for (let i = 0; i < data.length; i++) {
+    data[i][1] = replaceStockStatus(data[i][1]);
+  }
+
+  data.sort(function (tableRowA, tableRowB) {
+    const rocA = parseFloat(tableRowA[0]) || 0;
+    const rocB = parseFloat(tableRowB[0]) || 0;
+    return rocA - rocB;
+  });
+
+  if (data.length > 0) {
+    mySheet.getRange(1, 1, data.length, data[0].length).setValues(data);
+  }
+
+  function replaceStockStatus(value) {
+    if (!value) {
+      return null;
+    } else if (value === 'Inventory Status') {
+      return value;
+    } else if (value.toLowerCase().includes('in stock')) {
+      return 9999;
+    } else {
+      return 0;
+    }
+  }
+}
