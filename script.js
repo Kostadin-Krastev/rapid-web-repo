597640ce77b85fdf7a300f729c6a513024c4bb4f
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2025-10-13 13:54:28 +0300

    pull data, sort roc numbers from smallest to bigest and remove empty rows

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 function syncFromExternalSheet() {
   const externalSheetId = '1oOjqXsaCAjSOkA1lXrasNtUVtrsxvyxcFcolD8n6YXY';
   const externalSheetName = 'LOC1';
-
   const externalSource =
     SpreadsheetApp.openById(externalSheetId).getSheetByName(externalSheetName);
 
   const mySheetId = '1SQi7OMWiVyim7HTO7UhgdDZuzsJZAIXj2imoCiTAqRs';
   const mySheetName = 'Sheet1';
-
   const mySheet =
     SpreadsheetApp.openById(mySheetId).getSheetByName(mySheetName);
 
   const lastRow = externalSource.getLastRow();
   const numRows = lastRow - 8;
-  const data = externalSource.getRange(9, 2, numRows, 2).getValues();
+  let data = externalSource.getRange(9, 2, numRows, 2).getValues();
+
+  data = data.filter((row) => row[0] && row[0].toString().trim() !== ''); // премахвам празните редове в таблицата
 
   for (let i = 0; i < data.length; i++) {
     data[i][1] = replaceStockStatus(data[i][1]);
   }
 
-  mySheet.getRange(1, 1, data.length, data[0].length).setValues(data);
+  data.sort(function (tableRowA, tableRowB) {
+    const rocA = parseFloat(tableRowA[0]) || 0;
+    const rocB = parseFloat(tableRowB[0]) || 0;
+    return rocA - rocB;
+  });
+
+  if (data.length > 0) {
+    mySheet.getRange(1, 1, data.length, data[0].length).setValues(data);
+  }
 
   function replaceStockStatus(value) {
     if (!value) {
