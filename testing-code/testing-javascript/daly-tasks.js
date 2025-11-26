d110ed05091d06b3c95b90bbf39b28252e1585c0
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2025-11-26 22:23:12 +0200

    finishing the testing code

--- testing-code/testing-javascript/daly-tasks.js
+++ testing-code/testing-javascript/daly-tasks.js
@@ -2,4 +2,13 @@
 Format the result to 2 decimal places. toFixed(2)
 Use a fixed rate between the euro and lev: 1 EUR = 1.95583 BGN. Sample input: */
 
-const convertEuroToLev = function () {};
+let euro = 1;
+
+const calculateEuroToLev = function (euro) {
+  let oneLevValue = 1.95583;
+  const convertEuroToLev = euro * oneLevValue;
+
+  console.log(convertEuroToLev.toFixed(2));
+};
+
+calculateEuroToLev(euro);
