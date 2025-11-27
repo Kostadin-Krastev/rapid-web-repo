95d459203bffa9b62c53e5b6d292c1c8a65e6871
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2025-11-27 22:23:49 +0200

    start testing some js

--- testing-code/testing-javascript/daly-tasks.js
+++ testing-code/testing-javascript/daly-tasks.js
@@ -1,14 +1,9 @@
-/*Write a function to convert euro (EUR) to Bulgarian leva (BGN).
-Format the result to 2 decimal places. toFixed(2)
-Use a fixed rate between the euro and lev: 1 EUR = 1.95583 BGN. Sample input: */
+/*
+purchased 3 cabinets. 
+= print the total cost of the three cabinets =
+One cabinet cost $n, 
+the second was 20% cheaper than the first, 
+the third cost 15% more than the other two combined.
 
-let euro = 1;
-
-const calculateEuroToLev = function (euro) {
-  let oneLevValue = 1.95583;
-  const convertEuroToLev = euro * oneLevValue;
-
-  console.log(convertEuroToLev.toFixed(2));
-};
-
-calculateEuroToLev(euro);
+The output must be rounded to the third decimal point. .toFixed(3)
+ */
