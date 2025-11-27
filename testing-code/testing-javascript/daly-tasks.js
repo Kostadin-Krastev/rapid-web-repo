7b4873c679ec483c12595788e87a5becd7a49201
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2025-11-27 22:57:21 +0200

    create function for each cabinet price

--- testing-code/testing-javascript/daly-tasks.js
+++ testing-code/testing-javascript/daly-tasks.js
@@ -7,3 +7,13 @@ the third cost 15% more than the other two combined.
 
 The output must be rounded to the third decimal point. .toFixed(3)
  */
+
+const calculateEachCabinetCost = function(inputPrice) {
+let oneCabinetCost = inputPrice;
+const secondCabinetCost = oneCabinetCost * 0.8;
+const thirdCabinetCost = (oneCabinetCost + secondCabinetCost) * 1.15;
+};
+
+const calculateTotalCabinetPrice = function {
+  
+}
