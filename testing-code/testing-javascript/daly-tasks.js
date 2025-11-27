711484d9e87ed65e75889d2dd4d05b96524190fc
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2025-11-27 23:16:30 +0200

    finishing the test

--- testing-code/testing-javascript/daly-tasks.js
+++ testing-code/testing-javascript/daly-tasks.js
@@ -1,19 +1,16 @@
+'use strict';
 /*
-purchased 3 cabinets. 
-= print the total cost of the three cabinets =
-One cabinet cost $n, 
-the second was 20% cheaper than the first, 
-the third cost 15% more than the other two combined.
-
-The output must be rounded to the third decimal point. .toFixed(3)
+For the furnishing of the office purchased 3 cabinets. One cabinet cost $n, the second was 20% cheaper than the first, and the third cost 15% more than the other two  combined. 
+On the console print the total cost of the three cabinets – a real number. The output must be rounded to the third decimal point. 
  */
 
-const calculateEachCabinetCost = function(inputPrice) {
-let oneCabinetCost = inputPrice;
-const secondCabinetCost = oneCabinetCost * 0.8;
-const thirdCabinetCost = (oneCabinetCost + secondCabinetCost) * 1.15;
+const calculateEachCabinetCost = function (inputPrice) {
+  let oneCabinetCost = inputPrice;
+  const secondCabinetCost = oneCabinetCost * 0.8;
+  const thirdCabinetCost = (oneCabinetCost + secondCabinetCost) * 1.15;
+  const totalPrice = oneCabinetCost + secondCabinetCost + thirdCabinetCost;
+
+  console.log(`Total price of the tree cabinets is ${totalPrice.toFixed(3)}`);
 };
 
-const calculateTotalCabinetPrice = function {
-  
-}
+calculateEachCabinetCost(720.5);
